35bcbb6ab406b4391cf1cb3c1d7dd66481fc3519
ginooliver <gino.oliver@cloudsherpas.com>
2017-10-02 02:12:13 +0800

    quiz11-js-stalker

---
+++ quiz11-js-stalker/main.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your Firstname?");
+var lastName = prompt("What is your Lastname?");
+var age = prompt("What is your Age?");
+
+var msg1 = "You are " + firstName + " " + lastName + ".";
+var msg2 = "You are " + age + " years old."
+
+console.log(msg1);
+console.log(msg2);
