1ee74336e4f39ff7322e62cb364c574fe21f9634
ginooliver <gino.oliver@cloudsherpas.com>
2017-10-17 00:54:01 +0800

    quiz18-jqueryexercise

---
+++ quiz18-jqueryexercise/main.js
@@ -0,0 +1,13 @@
+$('div').css({'background-color':'purple'});
+
+$('div.highlight').css({'width':'200px'});
+
+$('div#third').css({'border': '1px solid orange'});
+
+$('div:first-of-type').css({'color':'pink'});
+
+// .text(); .text("new text");
+// .html(); .html("<tag>text</tag>");
+// .attr(); .attr("tag", "value"); .attr({tag1:"value2", tag2:"value2"});
+// .val(); .val("new value for inputs");
+
