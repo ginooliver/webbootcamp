e426af764110b328c6df5cf8def8b49315894607
ginooliver <dev.ginooliver@gmail.com>
2017-10-19 01:03:03 +0800

    add jquery reference

--- quiz18-jqueryexercise/main.js
+++ quiz18-jqueryexercise/main.js
@@ -6,8 +6,5 @@ $('div#third').css({'border': '1px solid orange'});
 
 $('div:first-of-type').css({'color':'pink'});
 
-// .text(); .text("new text");
-// .html(); .html("<tag>text</tag>");
-// .attr(); .attr("tag", "value"); .attr({tag1:"value2", tag2:"value2"});
-// .val(); .val("new value for inputs");
+
 
